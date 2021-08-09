9ec0e70024bb18c283dcec117e94551da1716c7c
Khushboo Bothra <bothrakhushboo2405@gmail.com>
2021-08-09 21:27:03 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TaskList from './TaskList';
+import TaskListContextProvider from '../context/TaskListContext';
+import '../App.css';
+import TaskForm from './TaskForm';
+import Header from './Header';
+
+const App = () => {
+    return (
+        <TaskListContextProvider>
+            <div className="container">
+                <div className="app-wrapper">
+                    <Header />
+                    <div className="main">
+                        <TaskForm />
+                        <TaskList />
+                    </div>
+                </div>
+            </div>
+        </TaskListContextProvider>
+
+    );
+};
+
+export default App;
